c51c43759f533e29cc7ff3164358ddfa697c9071
Siyu Guo <enyacat@gmail.com>
2022-03-24 17:25:39 +1100

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
-import Landing from './pages/Landing';
-import styled from 'styled-components';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { Dashboard, Landing, Error, Register } from './pages'
+import styled from 'styled-components'
 
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
-  );
+    <BrowserRouter>
+      <nav>
+        <Link to='/'>Dashboard</Link>
+        <Link to='/register'>Register</Link>
+        <Link to='/landing'>Landing</Link>
+      </nav>
+
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/landing' element={<Landing />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,12 +1,14 @@
-import logo from '../assets/images/logo.svg';
-import main from '../assets/images/main.svg';
-import Wrapper from '../assets/wrappers/LandingPage';
+import main from '../assets/images/main.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+import { Logo } from '../components'
+import { Link } from 'react-router-dom'
+
 const Landing = () => {
   return (
     <Wrapper>
       <main>
         <nav>
-          <img src={logo} alt='jobify' className='logo' />
+          <Logo />
         </nav>
         <div className='container page'>
           <div className='info'>
@@ -21,13 +23,15 @@ const Landing = () => {
               sartorial deep v vexillologist yr, chicharrones copper mug. Pug
               readymade raclette heirloom thundercats VHS occupy snackwave.
             </p>
-            <button className='btn btn-hero'>Login/Register</button>
+            <Link to='/register' className='btn btn-hero'>
+              Login / Register
+            </Link>
           </div>
           <img src={main} alt='job hunt' className='img main-img' />
         </div>
       </main>
     </Wrapper>
-  );
-};
+  )
+}
 
-export default Landing;
+export default Landing
