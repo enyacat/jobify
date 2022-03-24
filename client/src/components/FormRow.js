96cc2d1f97fe27387c7ac49509d56b730943b15e
Siyu Guo <enyacat@gmail.com>
2022-03-24 22:01:48 +1100

    add login page

---
+++ client/src/components/FormRow.js
@@ -0,0 +1,19 @@
+const FormRow = ({ type, value, name, handleChange, labelText }) => {
+  return (
+    <div>
+      <div className='form-row'>
+        <label htmlFor={name} className='form-label'>
+          {labelText || name}
+        </label>
+        <input
+          type={type}
+          value={value}
+          name={name}
+          onChange={handleChange}
+          className='form-input'
+        />
+      </div>
+    </div>
+  )
+}
+export default FormRow
