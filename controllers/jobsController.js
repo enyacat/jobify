e1651005719625f8c602658df1d87d8eb654de69
Siyu Guo <enyacat@gmail.com>
2022-03-25 15:08:10 +1100

    add authController, jobController, authRoutes, jobsRoutes"

---
+++ controllers/jobsController.js
@@ -0,0 +1,16 @@
+const createJob = async (req, res) => {
+  res.send('create job')
+}
+const getAllJobs = async (req, res) => {
+  res.send('getAll jobs')
+}
+const updateJob = async (req, res) => {
+  res.send('update job')
+}
+const deleteJob = async (req, res) => {
+  res.send('delete job')
+}
+const showStats = async (req, res) => {
+  res.send('show stats')
+}
+export { createJob, deleteJob, getAllJobs, updateJob, showStats }
--- server.js
+++ server.js
@@ -7,20 +7,22 @@ dotenv.config()
 import connectDB from './db/connect.js'
 //routers
 import authRouter from './routes/authRoutes.js'
+import jobsRouter from './routes/jobsRoutes.js'
 
 //middleware
 import notFoundMiddleware from './middleware/not-found.js'
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Welcome!')
-})
-
 app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', jobsRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
+
+app.get('/', (req, res) => {
+  res.send('Welcome!')
+})
 const port = process.env.PORT || 5000
 
 const start = async () => {
