675ede7d0645c301a342c4d06a1840afdc8703e6
Siyu Guo <enyacat@gmail.com>
2022-03-25 17:27:23 +1100

    add error handling

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 import errorHandlerMiddleware from './middleware/error-handler.js'
 import dotenv from 'dotenv'
 dotenv.config()
+import 'express-async-errors'
 //db and authenticateUser
 import connectDB from './db/connect.js'
 //routers
@@ -11,7 +12,9 @@ import jobsRouter from './routes/jobsRoutes.js'
 
 //middleware
 import notFoundMiddleware from './middleware/not-found.js'
-
+app.get('/', (req, res) => {
+  res.send('Welcome!')
+})
 app.use(express.json())
 
 app.use('/api/v1/auth', authRouter)
@@ -20,9 +23,6 @@ app.use('/api/v1/jobs', jobsRouter)
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
-app.get('/', (req, res) => {
-  res.send('Welcome!')
-})
 const port = process.env.PORT || 5000
 
 const start = async () => {
