cc2af46c2e20b325beaaa2f14f9258eef86542be
Siyu Guo <enyacat@gmail.com>
2022-03-25 11:36:06 +1100

    add error and notFound middleware

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 import express from 'express'
+import errorHandlerMiddleware from './middleware/error-handler.js'
 const app = express()
+//middleware
+
+import notFoundMiddleware from './middleware/not-found.js'
+
+app.get('/', (req, res) => {
+  throw new Error('error')
+  res.send('Welcome!')
+})
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
