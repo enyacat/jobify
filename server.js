83af0e8d33e141c1260c82feb712ddb8f74d3587
Siyu Guo <enyacat@gmail.com>
2022-03-25 12:22:14 +1100

    set up authController and routes

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 import express from 'express'
-import errorHandlerMiddleware from './middleware/error-handler.js'
 const app = express()
-//middleware
+import errorHandlerMiddleware from './middleware/error-handler.js'
+import dotenv from 'dotenv'
+dotenv.config()
+//db and authenticateUser
+import connectDB from './db/connect.js'
+//routers
+import authRouter from './routes/authRoutes.js'
 
+//middleware
 import notFoundMiddleware from './middleware/not-found.js'
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
-  throw new Error('error')
   res.send('Welcome!')
 })
+
+app.use('/api/v1/auth', authRouter)
+
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 const port = process.env.PORT || 5000
 
-app.listen(port, () => {
-  console.log(`server is listening on ${port}...`)
-})
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL)
+    app.listen(port, () => {
+      console.log(`server is listening on ${port}...`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
