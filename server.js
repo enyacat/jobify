83ce55f709966dfdccfc4c97c1f94d355af0cddc
Siyu Guo <enyacat@gmail.com>
2022-03-25 11:10:45 +1100

    install nodemon express,change package.json and setup server.js

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log('server running...')
+import express from 'express'
+const app = express()
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  console.log(`server is listening on ${port}...`)
+})
